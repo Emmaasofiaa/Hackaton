0070dc9dfa6c746cf6b86b784038f41c39ae554e
Emmaasofiaa <105502158+Emmaasofiaa@users.noreply.github.com>
2022-07-15 11:22:34 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -7,33 +7,33 @@ from wtforms.validators import DataRequired, InputRequired
 
 class Tema(FlaskForm):
     tema = StringField('Börja med att välja en utmaning för ert hackaton.', validators= [DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Deltagare(FlaskForm):
     deltagare = IntegerField('Hur många kommer att delta?', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Längd(FlaskForm):
     längd = IntegerField('Hur många timmar finns till förfogande? Ange tiden i hela timmar.', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Budget(FlaskForm):
     budget = IntegerField('Vad är er budget?', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Plats(FlaskForm):
     plats = StringField('Var tänker ni ordna ert hackaton?', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Jury(FlaskForm):
     jury = IntegerField('Hur många jurymedlemmar ska ni ha?', validators=[DataRequired()])
     #jury =  RadioField('6. Ska ni ha en jury?', choices=['Ja', 'Nej'], validators=[InputRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class JuryNamn(FlaskForm):
     juryNamn = StringField('Vilka är era jurymedlemmar?', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
 
 class Språk(FlaskForm):
     språk = IntegerField('Hur många språk ingår i ert hackaton?', validators=[DataRequired()])
-    submit = SubmitField('Mata in')
+    submit = SubmitField('+')
